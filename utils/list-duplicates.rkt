a78a2c2609928d7ed090c7c41dcce0437eabacba
David Van Horn <dvanhorn@cs.umd.edu>
2015-01-16 11:58:53 -0500

    Typed list-duplicates.

--- utils/list-duplicates.rkt
+++ utils/list-duplicates.rkt
@@ -1,17 +1,31 @@
-#lang racket
-(require file/md5)
+#lang typed/racket
+(require typed/file/md5)
+;; List all duplicate files in given directory
+;; Unlikely but possible hash collision will give wrong answer.
+;; Reports newer duplicates.
 
-(define table
-  (make-hash))
+(: table (HashTable Bytes (Setof Path)))
+(define table (make-hash))
 
 (unless (zero? (vector-length (current-command-line-arguments)))
   (current-directory (vector-ref (current-command-line-arguments) 0)))
 
+(: mt (Setof Path))
+(define mt (set))
+
+(: update-table! (Bytes Path -> Void))
 (define (update-table! h p)
-  (hash-set! table h
-             (set-union
-              (set p)
-              (hash-ref table h (set)))))
+  (hash-set! table
+             h
+             (set-union (set p)
+                        (hash-ref table
+                                  h
+                                  (λ () mt)))))
+
+(: mod< (Path Path -> Boolean))
+(define (mod< p1 p2)
+  (< (file-or-directory-modify-seconds p1)
+     (file-or-directory-modify-seconds p2)))
 
 (for ([p (directory-list)]
       #:unless (directory-exists? p))
@@ -19,8 +33,6 @@
 
 (for ([s (in-hash-values table)]
       #:unless (= 1 (set-count s)))
-
-  (for ([p (rest (sort (set->list s) <
-                       #:key file-or-directory-modify-seconds))])
+  (for ([p (rest (sort (set->list s) mod<))])
     (display (path->complete-path p))
     (newline)))
