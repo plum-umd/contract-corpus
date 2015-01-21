ac7c21a0237d80598d6bf9e106df28f671cc9cf6
David Van Horn <dvanhorn@cs.umd.edu>
2015-01-21 14:15:21 -0500

    Ignore non-modules.

--- utils/exercise-module.rkt
+++ utils/exercise-module.rkt
@@ -1,11 +1,24 @@
 #lang racket
-(require try-scv-racket/eval)
+(require try-scv-racket/eval
+         "shared.rkt")
 
+;; Sexpr -> Void
 (define (try-module m)
-  (match-define (list-rest 'module m-name 'racket _) m)
-  (define ev (make-ev-rkt))
-  (ev m)  
-  (define-values (vals stxs) (ev `(module->exports '',m-name)))
-  (ev `(require ',m-name))
-  (for/list ([i (dict-ref (append vals stxs) 0)]) 
-    (ev `(contract-exercise ,(car i)))))
+  (match m
+    [(list-rest 'module m-name 'racket _)
+     (define ev (make-ev-rkt))
+     (ev m)  
+     (define-values (vals stxs) (ev `(module->exports '',m-name)))
+     (ev `(require ',m-name))
+     (for/list ([i (dict-ref (append vals stxs) 0)]) 
+       (ev `(contract-exercise ,(car i))))]
+    [_ (void)]))
+
+#;
+(for-each try-module
+          (read (open-input-file "../1420736099704.sch")))
+     
+
+  
+
+
