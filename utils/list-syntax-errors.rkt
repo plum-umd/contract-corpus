62ea5d9c135498ea6e37f768bc2935252821f06b
David Van Horn <dvanhorn@cs.umd.edu>
2015-01-22 15:40:09 -0500

    Create eval for each file.

--- utils/list-syntax-errors.rkt
+++ utils/list-syntax-errors.rkt
@@ -7,30 +7,17 @@
 (unless (zero? (vector-length (current-command-line-arguments)))
   (current-directory (vector-ref (current-command-line-arguments) 0)))
 
-
-(define ev (make-ev-rkt))
-
 (define (syntax-error? path)
+  (define ev (make-ev-rkt))
   (with-handlers ([exn:fail:syntax? (lambda (x) #t)]
                   [exn:break? raise]
-                  [exn? (lambda (x) #;(print x) #f)])
+                  [exn? (lambda (x) #f)])
     (for-each ev (read-all path))
     #f))
 
-
-(for ([p #;(list "1421630531813.sch") (directory-list)]
-      
+(for ([p (in-list (directory-list))]
       #:unless (directory-exists? p)
       #:when (racket-file? p)
       #:when (syntax-error? p))
-  
-  (if (equal? (path->complete-path p)
-              (path->complete-path "1421630531813.sch"))
-      (display "HERE")      
-      (display (path->complete-path p)))
+  (display (path->complete-path p))
   (newline))
-#|
-(print (directory-exists? "1421630531813.sch"))
-(print (racket-file? "1421630531813.sch"))
-(print (syntax-error? "1421630531813.sch"))
-|#
