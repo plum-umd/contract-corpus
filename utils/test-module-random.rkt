0484403350e4f2790310cb09998fe0fb81ec158c
cmentzer <cwolfmentzer@gmail.com>
2015-01-26 13:53:37 +0000

    added test-module-random and test-module-scv, see comments

---
+++ utils/test-module-random.rkt
@@ -0,0 +1,61 @@
+#lang racket
+;using try-scv-racket/evil as a temporary fix for bug
+(require try-scv-racket/ev
+         racket/sandbox
+         syntax/parse
+         "shared.rkt")
+
+;; Sexpr Ev -> Void
+(define (try-module ev m)
+  (define m1 (syntax->datum (hack-require-clause (datum->syntax #f m))))
+  (match m1
+    [(list-rest 'module m-name 'racket _)
+     (ev m1)  
+     (define-values (vals stxs) (ev `(module->exports '',m-name)))
+     (ev `(require ',m-name))
+     (for ([i (dict-ref (append vals stxs) 0)])
+       (ev `(contract-exercise ,(car i))))]
+    [_ (void)]))
+
+(define handle-contract-fail (make-parameter (λ (p) void)))
+(define handle-terminated    (make-parameter (λ (p) void)))
+(define handle-resource      (make-parameter (λ (p) void)))
+(define handle-fail          (make-parameter (λ (p) void)))
+
+(define (list-unsafe-modules path)
+  (for ([p (in-list (directory-list path #:build? #t))]
+	#:unless (directory-exists? p)
+	#:when (racket-file? p)
+	#:when (readable? p))
+    (with-handlers ([exn:fail:contract? ((handle-contract-fail) p)]
+		    ;; DVH: I don't think this should happen unless running in DrRacket
+		    #;[exn:fail:out-of-memory? (λ (x) (printf "~a~n" p))]
+		    [exn:break? raise]
+		    [exn:fail:sandbox-terminated? ((handle-terminated) p)]
+		    [exn:fail:resource? ((handle-resource) p)]
+		    [exn:fail? ((handle-fail) p)] ; covers `error`
+		    [exn? (λ (x) (printf "~a~a~n" p))]) ; raise anything not covered
+      (for-each (λ (x) (begin (define ev (make-ev-rkt))
+                              (try-module ev x)))
+                (read-all p)))))
+
+(define show
+  (λ (p) (λ (_) (printf "~a~n" p))))
+
+(module* main #f
+  (command-line
+   #:multi
+   [("--error")
+    "Report contract"
+    (handle-fail show)]
+   [("--contract")
+    "Report contract"
+    (handle-contract-fail show)]
+   [("--timeout")
+    "Report out of time"
+    (handle-terminated show)]
+   [("--memory")
+    "Report out of memory"
+    (handle-resource show)])
+
+  (list-unsafe-modules "/home/clay/contract-corpus/progs"))
---
+++ utils/test-module-scv.rkt
@@ -0,0 +1,83 @@
+#lang racket
+(require try-scv-racket/evil
+         racket/sandbox
+         syntax/parse
+         soft-contract/check
+         "shared.rkt")
+
+(define input
+  '((module min racket 
+      (provide/contract (min (-> real? real? real?)))
+      (define (min x y) 
+        (if (< x y) x y)))
+    (module argmin racket 
+      (provide/contract (argmin (-> (-> any/c number?) (cons/c any/c (listof any/c)) any/c)))
+      (require (submod ".." min))
+      (define (argmin f xs) 
+        (cond 
+          ((empty? (cdr xs)) 
+           (f (car xs))) 
+          (else (min (f (car xs)) 
+                     (argmin f (cdr xs)))))))))
+
+;; try-module applies the evaluator to a module
+;; Sexpr Ev -> Void
+(define (try-module ev m)
+  (ev m))
+
+;;Define handlers for different raised exceptions
+;;the first two are defined by phil in softcontract/check.rkt
+(define handle-contract-counterexample (make-parameter (λ (p) void)))
+(define handle-contract-maybe          (make-parameter (λ (p) void)))
+(define handle-contract-fail           (make-parameter (λ (p) void)))
+(define handle-terminated              (make-parameter (λ (p) void)))
+(define handle-resource                (make-parameter (λ (p) void)))
+(define handle-fail                    (make-parameter (λ (p) void)))
+
+(define (list-unsafe-modules path)
+  (for ([p (in-list (directory-list path #:build? #t))]
+        #:unless (directory-exists? p)
+        #:when (racket-file? p)
+        #:when (readable? p))
+    (with-handlers ([exn:fail:contract:counterexample? ((handle-contract-counterexample) p)]
+                    ;; DVH: I don't think this should happen unless running in DrRacket
+                    #;[exn:fail:out-of-memory? (λ (x) (printf "~a~n" p))]
+                    [exn:break? raise]
+                    [exn:fail:resource? ((handle-resource) p)]
+                    [exn:fail? (λ (x) (printf "~a ~a~n" p x))]
+                    ;[exn:fail? ((handle-fail) p)] ; covers `error`
+                    [exn? raise]) ; raise anything not covered
+      ;; for each module in a file, create an instance of an ev
+      ;; and use it to evaluate the module
+      (for-each (λ (x) (begin (define ev (make-ev))
+                             ; (printf "~a~n" x)
+                              (try-module ev (list x))))
+                (read-all p)))))
+
+(define show
+  (λ (p) (λ (_) (printf "~a~n" p))))
+
+(module* main #f
+  (command-line
+   #:multi
+   [("--counterexample")
+    "Report counterexample found"
+    (handle-contract-counterexample show)]
+   [("--maybe")
+    "Report maybe counterexample found"
+    (handle-contract-maybe show)]
+   [("--error")
+    "Report errors"
+    (handle-fail show)]
+   [("--contract")
+    "Report contract"
+    (handle-contract-fail show)]
+   [("--timeout")
+    "Report out of time"
+    (handle-terminated show)]
+   [("--memory")
+    "Report out of memory"
+    (handle-resource show)])
+  
+  (list-unsafe-modules "/home/clay/contract-corpus/progs"))
+
