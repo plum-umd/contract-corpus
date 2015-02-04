220c40d9486ee81690711335571df110428a57cb
Phil Nguyen <pcn@cs.umd.edu>
2015-02-04 14:12:51 -0500

    generate example name properly

--- utils/dump-examples-to-javascript.rkt
+++ utils/dump-examples-to-javascript.rkt
@@ -1,14 +1,20 @@
 #lang racket/base
-(require racket/contract racket/file racket/string racket/port racket/cmdline racket/match)
+(require racket/contract racket/file racket/string racket/port racket/cmdline racket/match racket/function)
 
 (define/contract (dump dir-name out-file)
   (path-string? path-string? . -> . void?)
   
   (define-values (labels contents)
     (for/lists (labels contents) ([file (in-directory dir-name)]
-                                  [i #|FIXME|# (in-naturals)]
                                   #:when (regexp-match-exact? ".*sch" (path->string file)))
-      (define label #|FIXME|# (format "ex-~a" i))
+      (define label ; remove `in-dir` prefix, convert `-` -> `_`, remove `.sch`
+        (regexp-replace
+         ".sch"
+         (regexp-replace
+          "-"
+          (regexp-replace (format "~a/" dir-name) (path->string file) "")
+          "_")
+         ""))
       (define content
         (with-output-to-string
             (λ () (write (format "~a~n" (file->string file))))))
@@ -17,9 +23,7 @@
   (with-output-to-file out-file
     (λ ()
       (printf
-       (string-join (for/list ([label labels]
-                               [content contents])
-                      (format "~a = ~a" label content))
+       (string-join (map (curry format "~a = ~a") labels contents)
                     "\n,\n"
                     #:before-first "var examples = {\n"
                     #:after-last "\n}")))
